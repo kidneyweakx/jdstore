c3d23ed510af239056e60ff9fa73330d56bd8ab9
kidneyweakx <kidneyweakx@gmail.com>
2019-05-14 11:54:56 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,11 @@ Rails.application.routes.draw do
     end
   end
   resources :cart_items
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_eth
+    end
+  end
   namespace :account do
     resources :orders
   end
